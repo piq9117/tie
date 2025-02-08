71aedbc586d2748d8af0b76ab6713dafda88f4c0
piq9117 <piq9117@gmail.com>
2025-02-08 00:25:17 -0500

    add octem-stream

--- src/Tie/Operation.hs
+++ src/Tie/Operation.hs
@@ -326,7 +326,8 @@ requestBodyToRequestBody resolver Errors {..} requestBody = do
       ( asum
           [ InsOrd.lookup "application/json" (OpenApi._requestBodyContent requestBody),
             InsOrd.lookup "application/x-ndjson" (OpenApi._requestBodyContent requestBody),
-            InsOrd.lookup "application/x-tar" (OpenApi._requestBodyContent requestBody)
+            InsOrd.lookup "application/x-tar" (OpenApi._requestBodyContent requestBody),
+            InsOrd.lookup "application/octet-stream" (OpenApi._requestBodyContent requestBody)
           ]
       )
       (traceShow requestBody $ unsupportedMediaType)
